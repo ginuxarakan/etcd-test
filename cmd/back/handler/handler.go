3a306afad1841057b49ae24ca6e017a2c5577ab4
maroo <mgoothan.psnl@gmail.com>
2022-12-31 14:32:14 +0630

    setup handler and register router with gin and setup http server close #6 close #7

--- cmd/back/handler/handler.go
+++ cmd/back/handler/handler.go
@@ -1,1 +1,28 @@
 package handler
+
+import "github.com/gin-gonic/gin"
+
+type Handler struct {
+	R *gin.Engine
+}
+
+type HConfig struct {
+	R *gin.Engine
+}
+
+func NewHandler(c *HConfig) (*Handler, error) {
+	return &Handler{
+		R: c.R,
+	}, nil
+}
+
+func (h *Handler) Register() {
+	h.R.GET(
+		"/",
+		func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"data": "Success",
+			})
+		},
+	)
+}
